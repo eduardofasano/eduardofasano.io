cbb0f8ffce7fd20c8bd7c71755fb94575cf030d2
eduardofasano <eduardo.fasano.93@gmail.com>
2017-02-25 19:30:56 +0000

    sg with cosmo help

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
+const sg = require('sendgrid')('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');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,3 +21,46 @@ app.use(router);
 // app.use(sendgrid);
 
 app.listen(port, () => console.log(`Running on port André ${port} 🎙`));
+
+app.post('/email', function(req, res) {
+  //SENDGRID STUFF
+
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: {
+      personalizations: [
+        {
+          to: [
+            {
+              email: 'eduardo.fasano.93@gmail.com',
+            },
+          ],
+          subject: 'Hello World from the SendGrid Node.js Library!',
+        },
+      ],
+      from: {
+        email: 'efasano@bu.edu',
+      },
+      content: [
+        {
+          type: 'text/plain',
+          value: 'Hello, Email!',
+        },
+      ],
+    },
+  });
+
+  sg.API(request)
+  .then(response => {
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  })
+  .catch(error => {
+    //error is an instance of SendGridError
+    //The full response is attached to error.response
+    console.log("FAILED")
+    console.log(error.response.body);
+  });
+});
--- src/js/controllers/emailCtrl.js
+++ src/js/controllers/emailCtrl.js
@@ -1,4 +1,3 @@
 // angular.module('eduardo-portfolio')
 //   .controller('EmailNewController', EmailNewController);
 //
-// EmailNewController
